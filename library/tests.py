098eade8462cbf1db452e79770de0b39ad43cf7f
Andre Martins <anndreh@gmail.com>
2026-03-27 12:27:45 -0300

    Adds some tests just to test

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class ValidateView(SimpleTestCase):
+    
+    def test_return_after_create(self):
+        client = APIClient()
+        res = client.get('/api/books/')
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(
+            res.data,
+            [{"id":1,"first_name":"Marcus","last_name":"Aurelius","biography":""}]
+        )
